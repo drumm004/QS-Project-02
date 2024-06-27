bcacc3ddcaa5fa44ca4769a957144e86caa37979
drumm004 <david.s.rummel@gmail.com>
2024-06-27 18:52:56 -0400

    got images to display with JS and cleaned up some css

--- scripts/project_2_script.js
+++ scripts/project_2_script.js
@@ -12,8 +12,36 @@ const API_KEY="KsZLDr69nDEELdE1IQEZmZAtpXRbSKlz"
             .then((response) => response.json())
             .then((json) => {
                 console.log(json);
-                const data = json.data.images.fixed_height.url;
-                elementOutputArea.innerHTML = data; 
-                
+                const data = json.data;
+                console.log(data);
+                const url = json.data.images.fixed_height.url;
+                console.log(url);
+                let img=`<img src="${url}" alt="${data.title}" />`
+                //elementOutputArea.innerHTML = `<div>${JSON.stringify(data.title)}</div>`;
+                elementOutputArea.innerHTML = img; 
             });
     }
+
+    function handleClickArray() {
+        console.log("handleClickArray called");   
+        //const search = "yes";
+        const elementOutputArea = document.getElementById("outputArea");
+        const search = document.getElementById("search").value;
+        let url = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${API_KEY}&limit=${PAGE_SIZE}&offset=${offset}`
+        console.log('url=' + url);
+
+        fetch(url, {method: "GET"})
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json);
+                console.log(json.pagination);
+                elementOutputArea.innerHTML = "";
+                for (let data of json.data) {
+                    //let tag = `<div>${JSON.stringify(data.title)}</div>`
+                    const url = data.images.fixed_height.url;
+                    let img=`<img src="${url}" alt="${data.title}" />`
+                    elementOutputArea.innerHTML += img;
+                    
+            }
+        });
+}
