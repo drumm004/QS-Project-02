249200ae2ef501c96e572c29bf64da43e2d598bf
drumm004 <david.s.rummel@gmail.com>
2024-06-27 01:23:02 -0400

    added script code and an arbitrary output page

---
+++ scripts/project_2_script.js
@@ -0,0 +1,18 @@
+const API_KEY="lSAMJWItJZzHYeXvlctHi3ebBgyDckAh"
+    const PAGE_SIZE=10;
+    let offset=0;
+    function handleClickRandom() {
+        console.log("handleClickRandom called");   
+        const search = "api.giphy.com/v1/gifs/random"; 
+        const elementOutputArea = document.getElementById("outputArea");
+        let url = `http://${search}?api_key=${API_KEY}`;
+        console.log('url=' + url);
+
+        fetch(url, {method: "GET"})
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json);
+                const data = json.data.images.fixed_height.url;
+                elementOutputArea.innerHTML = `<div>${JSON.stringify(data)}</div>`;
+            });
+    }
